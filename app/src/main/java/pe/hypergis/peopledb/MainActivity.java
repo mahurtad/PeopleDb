d9f4e3a30b2c231d1035f9d2768348939e0f28f6
Manuel HA <nicamis@gmail.com>
2017-12-02 11:32:35 -0500

    Implementando Firebase

--- app/src/main/java/pe/hypergis/peopledb/MainActivity.java
+++ app/src/main/java/pe/hypergis/peopledb/MainActivity.java
@@ -1,12 +1,17 @@
 package pe.hypergis.peopledb;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
+
+import com.github.clans.fab.FloatingActionMenu;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +20,16 @@ import pe.hypergis.peopledb.model.Person;
 import pe.hypergis.peopledb.model.PersonDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
     private List<Person> items = new ArrayList();
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager lManager;
+    FloatingActionMenu materialDesignFAM;
+    com.github.clans.fab.FloatingActionButton floatingActionButton1, signOut;
 
 
     @Override
@@ -27,10 +38,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FillPersons();
 
+        auth = FirebaseAuth.getInstance();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
         // Obtener el Recycler
         recycler = (RecyclerView) findViewById(R.id.reciclador);
         recycler.setHasFixedSize(true);
-
         // Usar un administrador para LinearLayout
         lManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(lManager);
@@ -38,6 +66,35 @@ public class MainActivity extends AppCompatActivity {
         adapter = new PersonAdapter(items);
         recycler.setAdapter(adapter);
 
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
+        signOut = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.signOut);
+
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu first item clicked
+
+                Intent iconIntent = new Intent(v.getContext(), NewActivity.class);
+                v.getContext().startActivity(iconIntent);
+
+            }
+        });
+
+//importante para signout
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+
+
+
+
+
+/*
         FloatingActionButton btnAgregar = (FloatingActionButton) findViewById(R.id.btnAgregar);
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,12 +103,43 @@ public class MainActivity extends AppCompatActivity {
                 view.getContext().startActivity(iconIntent);
             }
         });
+*/
     }
+    public void signOut() {
+        auth.signOut();
+
+        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+    }
+
+
+
+
 
     private void FillPersons(){
         PersonDbHelper db = new PersonDbHelper(MainActivity.this);
         items = db.getAllPersons();
     }
+
+
+//importante para sign out
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+
 
     @Override
     public void onRestart()
